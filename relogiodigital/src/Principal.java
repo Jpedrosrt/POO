e77911b0c4265abb0f084a2c673dc5bd855c264c
João Pedro <j0310pedro@gmail.com>
2022-08-18 12:56:09 -0300

    Initial commit

---
+++ relogiodigital/src/Principal.java
@@ -0,0 +1,26 @@
+public class Principal {
+    public static void main(String[] args) {
+        RelogioDigital relogio1 = new RelogioDigital();
+        //-----------------------
+        String telaString = relogio1.getTempo();
+        
+        System.out.println( telaString );
+
+        relogio1.setTempo(15, 25);
+        
+        relogio1.tempoTic();
+
+        //-----------------------
+        telaString = relogio1.getTempo();
+
+        System.out.println( telaString );
+
+        relogio1.tempoTic();
+
+        //-----------------------
+        telaString = relogio1.getTempo();
+
+        System.out.println( telaString );
+
+    }
+}
